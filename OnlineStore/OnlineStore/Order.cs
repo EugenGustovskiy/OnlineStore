e86cbfae4048a36b065158e7f80e2a6d41977147
Eugen Gustovskiy <eu.hustouski@gmail.com>
2023-11-01 10:18:20 +0200

    add class Order

---
+++ OnlineStore/OnlineStore/Order.cs
@@ -0,0 +1,23 @@
+﻿namespace OnlineStore
+{
+    public class Order
+    {
+        public string ProductName { get; set; }
+        public float ProductPrice { get; set; }
+        public long CustomerNumber { get; set; }
+        public string DeliveryAddress { get; set; }
+
+        public Order(string productName, float productPrice, long customerName, string deliveryAddress)
+        {
+            ProductName = productName;
+            ProductPrice = productPrice;
+            CustomerNumber = customerName;
+            DeliveryAddress = deliveryAddress;
+        }
+
+        public string GetFullInformation()
+        {
+            return $"Product name: {ProductName}, Product price: {ProductPrice} BYN, Customer number: {CustomerNumber}, Delivery address: {DeliveryAddress}";
+        }
+    }
+}
---
+++ OnlineStore/OnlineStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using OnlineStore;
+
+Order order1 = new ("Xiaomi MI TV", 1329, 375336746162, "Minks, Odintsova, 21, 42");
+Order order2 = new ("Apple iPhone 14", 3079, 375298561972, "Minks, Kuntsevschina, 11, 32");
+Order order3 = new ("Sony PlayStation 5", 2259, 375447412596, "Minks, Kuntsevschina, 11, 32");
+
+Order[] orders = { order1, order2, order3 };
+
+foreach (Order order in orders)
+{
+    Console.WriteLine(order.GetFullInformation());
+}
